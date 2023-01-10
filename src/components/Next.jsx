2e2c2dbc648612e69c88919e7253bd67df855bb2
Emmanuel Dindu <emmanuelnwobodoc04@gmail.com>
2023-01-11 00:31:25 +0100

    👌 IMPROVE:

--- src/components/Next.jsx
+++ src/components/Next.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import svg from '../assets/Speed.png'
 
 function Next() {
   return (
-      <div className='bg-[#dbd5d5] h-screen'>
-          home page
+    <div className='bg-[#f1f1f1] h-screen overflow-hidden w-full '>
       
+      <div className="h-4/5 w-full mx-auto">
+        
+        <h1 className='mx-auto  relative'>
+          QuizSet
+        </h1>
+
+      <div className="w-full mx-auto sm:w-3/5 my-auto relative ">
+          <img src={svg}
+            alt=""
+            
+          
+          />
+              
+           </div>
+      
+      </div>
+       
     </div>
   )
 }
