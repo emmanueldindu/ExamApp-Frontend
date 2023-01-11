97e5b006b60d2a27597313a62a345174b00c8c00
Emmanuel Dindu <emmanuelnwobodoc04@gmail.com>
2023-01-11 15:08:10 +0100

    👌 IMPROVE:

--- src/components/Next.jsx
+++ src/components/Next.jsx
@@ -1,33 +1,39 @@
 import React from 'react'
 import svg from '../assets/Speed.png'
+import { Link } from 'react-router-dom';
 
 function Next() {
   return (
-    <div className='bg-[#f1f1f1] h-screen overflow-hidden flex-col w-full max-h-full '>
+    <div className='bg-[#f1f1f1] h-screen  overflow-x-hidden flex-col w-full max-h-full sm:h-screen md:h-screen lg:h-screen '>
       
-      <div className="h-3/4 w-full relative flex-col items-center mt-12 py-6  justify-center  mx-auto md:py-3  ">
-        <div>         
-        <h1 className='relative mx-auto pb-12 px-36  w-full sm:px-60 '>
+      <div className="h-4/5 w-full relative items-center  py-6  justify-center  mx-auto md:py-3   ">
+        <div className='w-full relative mx-auto justify-center items-center text-center'>         
+        <h1 className='relative mx-auto pb-12 px-36 title text-3xl underline underline-offset-8 decoration-yellow-500 gap-2   w-full sm:w-full  '>
           QuizSet
         </h1>
         </div>
 
-      <div className="w-full mx-auto h-full sm:w-3/5  relative  ">
+      <div className="w-full mx-auto h-4/5 absolute  sm:w-full md:w-full    ">
           <img src={svg}
             alt=""
+            className='h-3/4 md:h-4/5 relative mx-auto md:px-8 '
             
           
           />
           <div className='mx-auto relative items-center text-center'>
             <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, ipsam!
+              Do you have what it takes to battle in our Quiz test?? 😉
+              lets find out!!!
             </p>
           </div>
 
-          <div className="">
-            <button className='bg-black text-yellow-400'>
+          <div className=" relative w-full mx-auto items-center justify-center text-center pt-12 ">
+          <Link to="/main">
+            <button className='bg-black text-yellow-400 items-center text-center relative mx-auto btnn '>
               Next
-            </button>
+              </button>
+              </Link>
+
           </div>
               
            </div>
