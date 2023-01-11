718c64e98bb184c341b1bba8726d77e42fe31e71
Emmanuel Dindu <emmanuelnwobodoc04@gmail.com>
2023-01-11 10:05:08 +0100

    👌 IMPROVE:

--- src/components/Next.jsx
+++ src/components/Next.jsx
@@ -3,20 +3,32 @@ import svg from '../assets/Speed.png'
 
 function Next() {
   return (
-    <div className='bg-[#f1f1f1] h-screen overflow-hidden w-full '>
+    <div className='bg-[#f1f1f1] h-screen overflow-hidden flex-col w-full max-h-full '>
       
-      <div className="h-4/5 w-full mx-auto">
-        
-        <h1 className='mx-auto  relative'>
+      <div className="h-3/4 w-full relative flex-col items-center mt-12 py-6  justify-center  mx-auto md:py-3  ">
+        <div>         
+        <h1 className='relative mx-auto pb-12 px-36  w-full sm:px-60 '>
           QuizSet
         </h1>
+        </div>
 
-      <div className="w-full mx-auto sm:w-3/5 my-auto relative ">
+      <div className="w-full mx-auto h-full sm:w-3/5  relative  ">
           <img src={svg}
             alt=""
             
           
           />
+          <div className='mx-auto relative items-center text-center'>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, ipsam!
+            </p>
+          </div>
+
+          <div className="">
+            <button className='bg-black text-yellow-400'>
+              Next
+            </button>
+          </div>
               
            </div>
       
