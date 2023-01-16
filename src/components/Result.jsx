f960c7273749e628dfa0461a1e94b18c74583d0b
Emmanuel Dindu <emmanuelnwobodoc04@gmail.com>
2023-01-16 08:17:52 +0100

    result created

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 function Result() {
   return (
-    <div>
+    <div className='bg-[#f1f1f1] h-screen  overflow-x-hidden flex-col w-full max-h-full sm:h-screen md:h-screen lg:h-screen '>
+    <div className="h-4/5 w-full relative items-center  py-6  justify-center  mx-auto md:py-3   ">
       
-    </div>
+      <div className='w-full relative mx-auto justify-center items-center text-center'>         
+      <h1 className='relative mx-auto pb-12 px-36 title text-3xl underline underline-offset-8 decoration-yellow-500 gap-2   w-full sm:w-full  '>
+        QuizSet
+      </h1>
+      </div>
+ 
+    
+   
+
+      </div>
+  
+  </div>
   )
 }
 
