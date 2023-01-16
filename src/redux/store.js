1b174d8b43b4b342b1c09f44a201c55b10be8742
Emmanuel Dindu <emmanuelnwobodoc04@gmail.com>
2023-01-16 10:17:47 +0100

    state created

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import  questionReducer  from "./questionReducer";
+import { resultReducer } from "./resultReducer";
+
+const rootReducer = combineReducers({
+    questions: questionReducer,
+    result: resultReducer
+    
+
+})
+
+export default configureStore({reducer: rootReducer})
